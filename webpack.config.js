725c013f46d609d77f325a5b7c2b07eaf4670db8
Phạm Hải Đăng <Dang.PH@vietinbank.vn>
2024-11-07 20:57:06 +0700

    init

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.tsx', // Điểm vào ứng dụng sử dụng TypeScript
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/, // Thêm TypeScript (.ts, .tsx) vào Webpack
+        exclude: /node_modules/,
+        use: 'ts-loader',
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'], // Hỗ trợ .ts, .tsx
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    compress: true,
+    port: 3000,
+    hot: true,
+  },
+  mode: 'development',
+};
