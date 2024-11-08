8aebd0d0744d9143dc84db69365257908d5f20ad
Phạm Hải Đăng <Dang.PH@vietinbank.vn>
2024-11-08 09:17:00 +0700

    init

--- webpack.config.js
+++ webpack.config.js
@@ -13,8 +13,8 @@ module.exports = {
       {
         test: /\.(ts|tsx)$/, // Thêm TypeScript (.ts, .tsx) vào Webpack
         exclude: /node_modules/,
-        loader: 'ts-loader',
-        // use: 'ts-loader',
+        // loader: 'ts-loader',
+        use: 'ts-loader',
       },
       {
         test: /\.css$/,
